68dade77d83bcf7106f345e7659b9260db4c9418
MeasureO <fedotov.dmitry.sergeevich@gmail.com>
2021-12-30 04:34:34 +0300

    fix

---
+++ dominoes.py
@@ -0,0 +1,114 @@
+import random
+pieces = []
+for i in range(0, 7):
+    for j in range(0, 7):
+        if i <= j:
+            pieces.append([i, j])
+
+#print(len(pieces))
+random.shuffle(pieces)
+stock = pieces[:14]
+computer =  pieces[14:21]
+player = pieces[21:28]
+maximum = 0
+max_piece = [0, 0]
+for i in range(0, 7):
+    for j in range(0, 7):
+        if i == j and [i, i] in pieces[14:28] and i > maximum:
+            max_piece = [i, i]
+            
+if max_piece in computer:
+    computer.remove(max_piece)
+    status = "player"
+else:
+    player.remove(max_piece)
+    status = "computer"
+
+        
+    
+    
+board = [max_piece]
+while True:
+    print("=" * 70)
+    print("Stock size:", len(stock))
+    print("Computer pieces:", len(computer))
+    print()
+    if len(board) <= 6:
+        print(*board, sep='')
+    else:
+        print(*board[:3], sep='', end='')
+        print('...', end='')
+        print(*board[-3:], sep='')
+    print()
+    for i, elem in enumerate(player):
+        print(i + 1,':', elem, sep='')
+    print()
+    if status == "The game is over. It's a draw!":
+        print("Status:", "The game is over. It's a draw!")
+        break
+    elif status == "The game is over. You won!":
+        print("Status:", "The game is over. You won!")
+        break 
+    elif status == "The game is over. The computer won!":
+        print("Status:", "The game is over. The computer won!")
+        break
+    elif status == "player":
+        print("Status:",  "It's your turn to make a move. Enter your command.")
+    elif status == "computer":
+        print("Status:", "Computer is about to make a move. Press Enter to continue...")
+    while True:
+        command = input()
+        if status == "player" and command.isnumeric() and int(float(command)) == int(command) and -len(player) <= int(command) <= len(player):
+            command = int(command)
+            if command == 0:
+                choice = random.choice(stock)
+                player.append(choice)
+                stock.remove(choice)
+            elif command > 0:
+                board.append(player[command - 1])
+                del player[command - 1]
+                
+            elif command[0] == '-':
+                board.insert(0, player[command - 1])
+                del player[command - 1]
+            status = "computer"
+        elif status == "computer":
+            minus_move = '-'
+            plus_move = '+'
+            zero_move = '0'
+            move = [minus_move, plus_move, zero_move]
+            command = random.choice(move)
+            if command == '0':
+                choice = random.choice(stock)
+                computer.append(choice)
+                stock.remove(choice)
+            elif command == '+':
+                choice = random.choice(computer)
+                board.append(choice)
+                computer.remove(choice)
+                
+            elif command == '-':
+                choice = random.choice(computer)
+                board.insert(0, choice)
+                computer.remove(choice)
+            status = "player"
+        else:
+            print("Invalid input. Please try again.")
+            continue
+        break
+        
+    
+        
+    if len(stock) == 0:
+        status = "The game is over. It's a draw!"
+    if len(player) == 0:
+        status = "The game is over. You won!"
+    if len(computer) == 0:
+        status = "The game is over. The computer won!"
+        
+
+        
+        
+        
+        
+        
